b46bb72098466d292396584d5698b2795d364571
Deepaksai1919 <saideepak220@gmail.com>
2021-04-12 20:56:16 +0530

    Initial commit

---
+++ scripts/enableAndroidX.js
@@ -0,0 +1,65 @@
+const fs = require('fs');
+const PLUGIN_NAME = 'newPlugin';
+
+const str = 'android.useAndroidX';
+const gradelPropertiesPath = './platforms/android/gradle.properties';
+
+function log(message) {
+  console.log(PLUGIN_NAME + ': ' + message);
+}
+
+function onError(error) {
+  log('ERROR: ' + error);
+}
+
+function run() {
+  let gradleProperties = fs.readFileSync(gradelPropertiesPath);
+  if (gradleProperties) {
+    let updatedGradleProperties = false;
+    gradleProperties = gradleProperties.toString();
+    if (gradleProperties.includes(`${str}=true`)) {
+      return;
+    }
+    if (gradleProperties.includes(`${str}=false`)) {
+      gradleProperties = gradleProperties.replace(
+        `${str}=false`,
+        `${str}=true`
+      );
+      updatedGradleProperties = true;
+    } else if (!gradleProperties.includes(str)) {
+      gradleProperties = gradleProperties.trimEnd();
+      gradleProperties += '\n' + str + '=true';
+      updatedGradleProperties = true;
+    } else if (!gradleProperties.includes(`${str}=true`)) {
+      gradleProperties = gradleProperties.split('\n');
+      for (var i = 0; i < gradleProperties.length; i++) {
+        if (gradleProperties[i].includes(str)) {
+          let prop = gradleProperties[i].split('=');
+          prop[1] = 'true';
+          gradleProperties[i] = prop.join('=');
+          break;
+        }
+      }
+      gradleProperties = gradleProperties.join('\n');
+      updatedGradleProperties = true;
+    }
+
+    if (updatedGradleProperties) {
+      fs.writeFileSync(gradelPropertiesPath, gradleProperties, 'utf-8');
+    }
+  } else {
+    log('gradle.properties file is not found');
+  }
+}
+
+module.exports = function () {
+  return new Promise((resolve, reject) => {
+    try {
+      run();
+      resolve();
+    } catch (e) {
+      onError('Exception: ' + e.toString());
+      reject(e);
+    }
+  });
+};
---
+++ www/cordova-truecaller.js
@@ -0,0 +1,98 @@
+var exec = require('cordova/exec');
+
+function getColor(color) {
+    return color;
+}
+const loginTextPrefix = {
+    'To get started': 0,
+    'To continue': 1,
+    'To place order': 2,
+    'To complete your order': 3,
+    'To checkout': 4,
+    'To complete your booking': 5,
+    'To proceed with your booking': 6,
+    'To continue with your booking': 7,
+    'To get details': 8,
+    'To view more': 9,
+    'To continue reading': 10,
+    'To proceed': 11,
+    'For new updates': 12,
+    'To get updates': 13,
+    'To subscribe': 14,
+    'To subscribe and get updates': 15,
+};
+
+const loginTextSuffix = {
+    'Please verify mobile no': 0,
+    'Please login': 1,
+    'Please signup': 2,
+    'Please login signup': 3,
+    'Please register': 4,
+    'Please sign in': 5,
+};
+
+const ctaTextPrefix = {
+    'Use': 0,
+    'Continue with': 1,
+    'Proceed with': 2,
+};
+
+const consentMode = {
+    'Popup': 4,
+    'Fullscreen': 8,
+    'Bottomsheet': 128,
+};
+
+const consentTitle = {
+    'Login': 0,
+    'Sign up': 1,
+    'Sign in': 2,
+    'Verify': 3,
+    'Register': 4,
+    'Get started': 5,
+};
+
+const buttonShape = {
+    'Rounded': 1024,
+    'Rectangle': 2048,
+};
+
+const footerType = {
+    'Another method': 256,
+    'Manually': 512,
+    'Later': 4096,
+};
+
+const sdkOption = {
+    'Without otp': 16,
+    'With otp': 32,
+};
+
+function generate(options) {
+    let op = {};
+    op['consentMode'] = consentMode[options.consentMode];
+    op['buttonColor'] = getColor(options.buttonColor);
+    op['buttonTextColor'] = getColor(options.buttonTextColor);
+    op['loginTextPrefix'] = loginTextPrefix[options.loginTextPrefix];
+    op['loginTextSuffix'] = loginTextSuffix[options.loginTextSuffix];
+    op['ctaTextPrefix'] = ctaTextPrefix[options.ctaTextPrefix];
+    op['buttonShapeOptions'] = buttonShape[options.buttonShape];
+    op['privacyPolicyUrl'] = options.privacyPolicyUrl;
+    op['termsOfServiceUrl'] = options.termsOfServiceUrl;
+    op['footerType'] = footerType[options.footerType];
+    op['consentTitleOption'] = consentTitle[options.consentTitle];
+    op['sdkOptions'] = sdkOption[options.sdkOptions];
+}
+
+exports.verify = function (options, success, error) {
+    options = generate(options);
+    exec(success, error, 'CordovaTruecaller', 'truecallerInit', [options]);
+};
+
+exports.check = function(success, error) {
+    exec(success, error, 'CordovaTruecaller', 'checkTruecaller', []);
+};
+
+exports.clearSdk = function() {
+    exec(null, null, 'CordovaTruecaller', 'clearSDk', []);
+};
